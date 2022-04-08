038cedcfcadd2db953083f236c5c1a90e8fb7155
Jakob Thunen <jthunen@dev-10.com>
2022-04-08 15:41:00 -0500

    started on game initialization

--- wheel_of_python.py
+++ wheel_of_python.py
@@ -17,3 +17,40 @@ def generate_wheel():
     
     return wheel_lst
     
+def generate_puzz_list():
+    puzzf = open("puzzles.txt", "rt")
+    puzz_list = puzzf.readlines()
+    puzzf.close
+    for i in range(len(puzz_list)):
+        puzz_list[i] = puzz_list[i].strip()
+    return puzz_list
+    
+def spin_wheel(wheel):
+    return random.choice(wheel)
+
+def main():
+    print("Welcome to Wheel of Python!")
+    print("To begin, please enter the names of the players:")
+    p1name = input("Player 1's name: ")
+    p2name = input("Player 2's name: ")
+    p3name = input("Player 3's name: ")
+    
+    playerscores = {
+        p1name : 0,
+        p2name : 0,
+        p3name : 0
+    }
+    
+    playerbanks = {
+        p1name : 0,
+        p2name : 0,
+        p3name : 0
+    }
+    
+    play_wheel = generate_wheel()
+    
+    puzzles_list = generate_puzz_list()
+    
+    cur_puzz = random.choice(puzzles_list)
+    
+main()
